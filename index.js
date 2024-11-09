8317a0b80b5063b0b6496944e6970ac6b266b9fb
tanneratkin <tmanatkin@gmail.com>
2024-11-09 12:45:10 -0700

    ejs update

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 let express = require("express");
-let app = new express();                                                                     
-
+let app = new express();
+app.set("view engine", "ejs");
 // set up database connection
 const knex = require("knex")({
- client: "mysql",
- connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "Password1",
-  database:"paradise-concerts",
-  port: 3306,
- },
+  client: "mysql",
+  connection: {
+    host: "concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "password",
+    database: "paradise-concerts",
+    port: 3306
+  }
 });
-
-app.get("/",(req,res) => {
- knex
- .select()
- .from("venues")
- .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+app.get("/", (req, res) => {
+  knex
+    .select()
+    .from("venues")
+    .then((result) => {
+      res.render("index", { aConcerts: result });
+    });
 });
 app.listen(3000);
